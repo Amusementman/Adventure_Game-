8e0aafe9d7918049860cc0a6155007db77076167
Amusementman man <harleytoshikian@gmail.com>
2024-12-10 19:48:47 +0000

    yes

--- Game.py
+++ Game.py
@@ -1,17 +1,13 @@
-
 def GAME(): 
     print("You were on a camping trip with your friends, it's been a hard week and you all deserve a break! But, you all ran out of berries, so you'r friends send you to find some berries.")
     print("Seems easy enough, so you head into the woods to find some berries.")
     print("However you didn't plan on how you'd find your way back to your campsite, so now you're horribly lost!")
     print("Let's hope you can get out...")
-    path = []
     FirstPath = int(input("descript...path 1 or 2: "))
-    path.append(FirstPath)
     if FirstPath == 1:
         def funk():
             print("stuff")
             SecondPath = int(input(""))
-            path.append(SecondPath)
             if SecondPath == 1:
                 print("s")
             elif SecondPath == 2:
