d392fa6a8212b02997a449100ee38ec4bc57e9db
Amusementman man <harleytoshikian@gmail.com>
2024-12-11 19:14:37 +0000

    yes

--- Game.py
+++ Game.py
@@ -1,8 +1,6 @@
 def GAME(): 
-    print("You were on a camping trip with your friends, it's been a hard week and you all deserve a break! But, you all ran out of berries, so you'r friends send you to find some berries.")
-    print("Seems easy enough, so you head into the woods to find some berries.")
-    print("However you didn't plan on how you'd find your way back to your campsite, so now you're horribly lost!")
-    print("Let's hope you can get out...")
+    inventory = ["Sword", "Healing potion", ""]
+    print("")
     FirstPath = int(input("descript...path 1 or 2: "))
     if FirstPath == 1:
         def funk():
